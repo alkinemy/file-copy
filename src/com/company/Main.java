48733e4e9c3e4e43af4489294b4928af0eb828fb
joke111 <alta.second@gmail.com>
2015-12-07 14:39:50 +0900

    file copy

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			try (InputStream inputStream = new BufferedInputStream(new FileInputStream("/Users/joke/Downloads/2015.12 온라인 과정 목록_공지용.xlsx"));
+				OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("/Users/joke/Downloads/xxxx" + i + ".xlsx"))) {
+				int data;
+				while ((data = inputStream.read()) != -1) {
+					outputStream.write(data);
+				}
+			}
+		}
+		long end = System.currentTimeMillis();
+
+		System.out.println(end - start);
+	}
+}
